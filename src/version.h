31f723a5764d8b96bcb4b8919d7f872213a3e80f
Dan Metcalf <daniel.metcalf@philips.com>
2016-04-28 16:57:17 -0400

    bump ver

--- src/version.h
+++ src/version.h
@@ -2,7 +2,7 @@
 #define VERSION
 
 #define XBRIDGE_VERSION_MAJOR 0
-#define XBRIDGE_VERSION_MINOR 37
+#define XBRIDGE_VERSION_MINOR 38
 #define XBRIDGE_VERSION_DESCR "rpc-btc-fix"
 
 #define MAKE_VERSION(major,minor) (( major << 16 ) + minor )
