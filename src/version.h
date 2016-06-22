9913e4023bd34680852a5fc0a4295dd8b7e2385e
corvaxx <maybe.mail@inbox.ru>
2016-06-22 14:28:14 +0300

    up version

--- src/version.h
+++ src/version.h
@@ -2,8 +2,8 @@
 #define VERSION
 
 #define XBRIDGE_VERSION_MAJOR 0
-#define XBRIDGE_VERSION_MINOR 44
-#define XBRIDGE_VERSION_DESCR "w-tax-2-fixed-with-1/2-ether"
+#define XBRIDGE_VERSION_MINOR 45
+#define XBRIDGE_VERSION_DESCR "ether-w-dht-ver-check"
 
 #define MAKE_VERSION(major,minor) (( major << 16 ) + minor )
 #define XBRIDGE_VERSION MAKE_VERSION(XBRIDGE_VERSION_MAJOR, XBRIDGE_VERSION_MINOR)
