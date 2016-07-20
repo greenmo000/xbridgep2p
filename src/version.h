b09c902c1bb2ba31d75840027e74306530a62aaf
corvaxx <maybe.mail@inbox.ru>
2016-07-20 15:31:22 +0300

    up version

--- src/version.h
+++ src/version.h
@@ -2,7 +2,7 @@
 #define VERSION
 
 #define XBRIDGE_VERSION_MAJOR 0
-#define XBRIDGE_VERSION_MINOR 50
+#define XBRIDGE_VERSION_MINOR 51
 #define XBRIDGE_VERSION_DESCR "rpc-http-server"
 
 #define MAKE_VERSION(major,minor) (( major << 16 ) + minor )
