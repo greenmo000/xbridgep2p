5ba0743729cd84ccf1027749335bba91c236003e
corvaxx <maybe.mail@inbox.ru>
2016-08-01 19:25:12 +0300

    up version

--- src/version.h
+++ src/version.h
@@ -2,7 +2,7 @@
 #define VERSION
 
 #define XBRIDGE_VERSION_MAJOR 0
-#define XBRIDGE_VERSION_MINOR 53
+#define XBRIDGE_VERSION_MINOR 54
 #define XBRIDGE_VERSION_DESCR "multicurrency"
 
 #define MAKE_VERSION(major,minor) (( major << 16 ) + minor )
